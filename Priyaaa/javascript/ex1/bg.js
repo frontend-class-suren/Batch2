2bcc9a9367a202131b03fd4f09eadff0f6b830bb
unknown <psaru434@gmail.com>
2024-10-21 11:27:50 +0545

    template

---
+++ Priyaaa/javascript/ex1/bg.js
@@ -0,0 +1,12 @@
+function bg(){
+    document.getElementById("bg").style.backgroundColor ="darkblue";
+    window.alert(' Do you want to change Background Successfully Changed..!')
+    //document.write('background change')
+    console.log("background change")
+}
+function removeBg(){
+ document.getElementById("bg").style.backgroundColor ="white";   
+}
+/*function imagebg(){
+    document.getElementById("bg").style.backgroundImage ="<img src='/boy.jpeg'>"
+}*/
