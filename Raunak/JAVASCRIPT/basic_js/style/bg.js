a11ecb281242082d792d8fd16b5cd44294163c92
Raunak Amatya <amatyaraunak56@gmail.com>
2024-10-23 11:24:31 +0545

    Javascript

---
+++ Raunak/JAVASCRIPT/basic_js/style/bg.js
@@ -0,0 +1,9 @@
+function bg(){
+    document.getElementById('bg').style.backgroundColor = "darkblue";
+    window.alert('Do you want to change the Background Color ?')
+    // document.write('Backgroud Changed') 
+    console.log('Background Changed')
+}
+function removebg(){
+    document.getElementById('bg').style.backgroundColor = "white";
+}
