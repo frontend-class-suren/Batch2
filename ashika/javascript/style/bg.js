725242b7a7375fb75a8cb55051928ccb9c1282e0
Ashika Aryal <ashikaaryal560@gmail.com>
2024-10-23 11:34:39 +0545

    java

---
+++ ashika/javascript/style/bg.js
@@ -0,0 +1,9 @@
+function bg(){
+document.getElementById("bg").style.backgroundColor="darkblue";
+window.alert('Do you want to chnage background?')
+// document.write('background changed')
+console.log('background change')
+}
+function removeBg(){
+document.getElementById("bg").style.backgroundColor="white";
+}
