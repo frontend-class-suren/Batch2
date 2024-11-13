99e7ebad21b1968d0a2e2e2cb85802ce3af8234e
unknown <psaru434@gmail.com>
2024-11-13 11:53:56 +0545

    javascript

---
+++ Priyaaa/javascript/js_hw/script.js
@@ -0,0 +1,32 @@
+
+function editor1(){
+    document.getElementById("textarea1").style.fontWeight = "bold";
+}    
+function editor2(){
+    document.getElementById("textarea1").style.fontStyle = "italic";
+}
+function editor3(){
+    document.getElementById("textarea1").style.textAlign = "left";
+}
+function editor4(){
+    document.getElementById("textarea1").style.textAlign = "center";
+}
+function editor5(){
+    document.getElementById("textarea1").style.textAlign = "right";
+}
+function editor6(){
+    document.getElementById("textarea1").style.textTransform = "uppercase";
+}
+function editor7(){
+    document.getElementById("textarea1").style.textTransform = "lowercase";
+}
+function editor8(){
+    document.getElementById("textarea1").style.textTransform = "capitalize";
+}
+function editor9(){
+    document.getElementById("textarea1").style.fontWeight = "normal";
+    document.getElementById("textarea1").style.textAlign = "left";
+    document.getElementById("textarea1").style.fontStyle = "normal";
+    document.getElementById("textarea1").style.textTransform = "capitalize";
+    document.getElementById("textarea1").value = "";
+}
