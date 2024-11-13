e3c57eb264839240f243b737db47b414e00f6d81
unknown <shresthasimran713@gmail.com>
2024-11-13 12:06:25 +0545

    hello

---
+++ Simraaan/Java Script/script.js
@@ -0,0 +1,45 @@
+
+function edit1(){
+    document.getElementById("textarea")
+    .style.fontWeight = "bold";
+}
+function edit2(){
+    document.getElementById("textarea")
+    .style.fontStyle = "italic";
+}
+function edit3(){
+    document.getElementById("textarea")
+    .style.textAlign = "left";
+}
+function edit4(){
+    document.getElementById("textarea")
+    .style.textAlign = "center";
+}
+function edit5(){
+    document.getElementById("textarea")
+    .style.textAlign = "right";
+}
+function edit6(){
+    document.getElementById("textarea")
+    .style.textTransform = "uppercase";
+}
+function edit7(){
+    document.getElementById("textarea")
+    .style.textTransform = "lowercase";
+}
+function edit8(){
+    document.getElementById("textarea")
+    .style.textTransform = "capitalize";
+}
+function edit9(){
+    document.getElementById("textarea")
+    .style.fontWeight = "normal";
+    document.getElementById("textarea")
+    .style.textAlign = "left";
+    document.getElementById("textarea")
+    .style.fontStyle = "normal";
+    document.getElementById("textarea")
+    .style.textTransform = "capitalize";
+    document.getElementById("textarea")
+    .value = "";
+}
