ed62a206debb48bdad6f681225b38f38f1950c33
unknown <psaru434@gmail.com>
2024-11-15 11:53:30 +0545

    js

--- Priyaaa/javascript/js_hw/clock.js
+++ Priyaaa/javascript/js_hw/clock.js
@@ -6,17 +6,17 @@ setInterval(showTime,1000);
     let sec = time.getSeconds();
     am_pm = "AM";
 
-    if(hour >=12){
-        if(hour > 12) hour -=12;
-        am_pm = "PM";
-    }
-    else if(hour == 0){
-        hr = 12;
-        am_pm = "AM";
-    }
-    // hour = hour < 10 ? "0"+ hour : hour;
-    // min = min < 10 ? "0"+ min : min;
-    // sec = sec < 10 ? "0"+ sec :sec;
+    // if(hour >=12){
+        // if(hour > 12) hour -=12;
+        // am_pm = "PM";
+    // }
+    // else if(hour == 0){
+        // hr = 12;
+        // am_pm = "AM";
+    // }
+    //  hour = hour < 10 ? "0"+ hour : hour;
+    //  min = min < 10 ? "0"+ min : min;
+    //  sec = sec < 10 ? "0"+ sec :sec;
 
     let currentTime = hour + ":"+ min + ":" + sec + am_pm;
 
