ae3d159be3123dbd6887917157360c87ed5b219a
unknown <psaru434@gmail.com>
2024-11-15 11:21:12 +0545

    js

---
+++ Priyaaa/javascript/js_hw/clock.js
@@ -0,0 +1,26 @@
+setInterval(showTime,1000);
+   function showTime(){
+    let time = new Date();
+    let hour = time.getHours();
+    let min = time.getMinutes();
+    let sec = time.getSeconds();
+    am_pm = "AM";
+
+    if(hour >=12){
+        if(hour > 12) hour -=12;
+        am_pm = "PM";
+    }
+    else if(hour == 0){
+        hr = 12;
+        am_pm = "AM";
+    }
+    // hour = hour < 10 ? "0"+ hour : hour;
+    // min = min < 10 ? "0"+ min : min;
+    // sec = sec < 10 ? "0"+ sec :sec;
+
+    let currentTime = hour + ":"+ min + ":" + sec + am_pm;
+
+    document.getElementById("clock").innerHTML = currentTime;
+    document.getElementById("clock").style.color = "blue";
+}
+// showTime();
