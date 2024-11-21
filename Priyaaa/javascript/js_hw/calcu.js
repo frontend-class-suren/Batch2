8c9c21bd6304730516528ae21253efbfb5888277
unknown <psaru434@gmail.com>
2024-11-21 12:01:57 +0545

    javascript

--- Priyaaa/javascript/js_hw/calcu.js
+++ Priyaaa/javascript/js_hw/calcu.js
@@ -1,15 +1,48 @@
-let input = document.getElementById("input");
-let buttons = document.querySelectorAll("button");
+let equal_pressed = 0;
+let button_input = document.querySelectorAll(".calc");
 
-let string = "";
-let arr = Array.from(buttons);
-arr.forEach(button => {
-    button.addEventListener('click', (e) => {
-        if(e.target.innerHTML == '='){
-            string = eval(string);
-            input.value = string;
+let input = document.getElementById("input");
+let equal = document.getElementById("equal");
+let clear = document.getElementById("clear");
+let erase = document.getElementById("erase");
+
+window.onload = () => {
+    input.value = "";
+};
+
+button_input.forEach(button_class => {
+    button_class.addEventListener("click", () => {
+        if(equal_pressed == 1){
+            input.value = "";
+            equal_pressed = 0;
         }
-        string += e.target.innerHTML;
-        input.value = string;
-    })
-})
+
+        input.value += button_class.value;
+    });
+});
+
+    equal.addEventListener("click", () => {
+      equal_pressed = 1;
+    let inp_val = input.value;
+    try{
+        let solution = eval(inp_val);
+
+        if(Number.isInteger(solution)){
+            input.value = solution;
+        }
+        else{
+            input.value = solution.toFixed(2);
+        }
+    }
+    catch(err){
+        alert("Invalid Input");
+    }
+});
+
+clear.addEventListener("click", () =>{
+    input.value = "";
+});
+
+erase.addEventListener("click", () =>{
+    input.value = input.value.substr(0, input.value.length - 1);
+});
